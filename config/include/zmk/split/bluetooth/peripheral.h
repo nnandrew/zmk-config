04bceb8160f724cb1fe23c599b3646405149c592
Andrew Nguyen <andrewvnguyen@utexas.edu>
2023-01-03 01:32:05 -0600

    fixed for sure

--- config/include/zmk/split/bluetooth/peripheral.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2022 The ZMK Contributors
- *
- * SPDX-License-Identifier: MIT
- */
-
-#pragma once
-
-bool zmk_split_bt_peripheral_is_connected(void);
--- config/include/zmk/usb_hid.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2020 The ZMK Contributors
- *
- * SPDX-License-Identifier: MIT
- */
-
-#pragma once
-
-int zmk_usb_hid_send_report(const uint8_t *report, size_t len);
